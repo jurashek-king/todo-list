2dfb29435a4599b41892de1410301d60a3514f8b
Jirka Kral <jurashek.king@gmail.com>
2021-02-11 16:55:57 +0100

    Added compoments

---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function Form({setInputText, inputText, todos, setTodos}) {
+    const inputTextHandler = (event) => {
+        console.log(event.target.value);
+        setInputText(event.target.value);
+    };
+
+    const submitTodoHandler = (event) => {
+        event.preventDefault();
+        setTodos([...todos, {text: inputText, completed: false, id: Math.random() * 1000}]);
+        setInputText("");
+    };
+
+    return (
+        <form>
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    )
+}
+
+export default Form;
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "./Todo.js"
+
+
+function TodoList({todos, setTodos}) {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+          {todos.map((todo) => {
+              return <Todo key={todo.id} todo={todo} setTodos={setTodos} todos={todos}/>
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
